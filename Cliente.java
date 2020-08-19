d2f12a477494640e19a2ebd6ba6dec3f1dd36052
Miguel Alejandro Rojas Hernández <nomeacuerdox4@gmail.com>
2020-08-18 23:14:44 -0500

    Final

--- Cliente.java
+++ Cliente.java
@@ -1,69 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Miguel Alejandro Rojas Hernandez
+ */
 import java.net.*;
 import java.io.*;
 import java.security.*;
 import javax.crypto.*;
 
-public class ClienteCifrado
+public class Cliente
 {
- public static void main(String a[]) throws Exception
- {
-  Socket socket = null;
-  String peticion = null;
-  String respuesta = null;
-
-/*		        
-  System.out.println( "Generando la llave..." );
-  KeyGenerator keyGen = KeyGenerator.getInstance("DES");
-  keyGen.init(56);
-  Key clave = keyGen.generateKey();
-  System.out.println( "La llave del cliente es:" + clave );
-  System.out.println( "Llave generada!" );
-*/			
-
-  ObjectInputStream ois = new ObjectInputStream(new FileInputStream("llave.ser"));
-  Key clave = (Key)ois.readObject();
-  ois.close();
-  System.out.println( "La llave del cliente es:" + clave );
-
-  System.out.println("Me conecto al puerto 8000 del servidor");
-  socket = new Socket(a[0],8000);
-  BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-  peticion = br.readLine();
-  System.out.println("Teclado: " + peticion);
-  byte[] textoPlano = peticion.getBytes("UTF8");
-  System.out.println( "El mensaje pasado a bytes y a bits es:" );
-  bytesToBits( textoPlano );
-  System.out.println( "----------------------------------------------------" );
-  Cipher cifrar = Cipher.getInstance("DES/ECB/PKCS5Padding");
-  cifrar.init(Cipher.ENCRYPT_MODE, clave);
-  byte[] textoCifrado = cifrar.doFinal(textoPlano);
-  System.out.println( "El argumento ENCRIPTADO es:" );
-  System.out.println( new String(textoCifrado, "UTF8") );
-  System.out.println( "El argumento ENCRIPTADO pasado a bytes y a bits es:" );
-  bytesToBits( textoCifrado );
-  System.out.println( "----------------------------------------------------" );
-  DataOutputStream dos = new DataOutputStream( socket.getOutputStream());
-  dos.write(textoCifrado);
-  dos.close();
-  socket.close();
- }
-
- public static void bytesToBits( byte[] texto )
- {
-  StringBuilder stringToBits = new StringBuilder();
-  for( int i=0; i < texto.length; i++ )
-  {
-   StringBuilder binary = new StringBuilder();
-   byte b = texto[i];
-   int val = b;
-   for( int j = 0; j < 8; j++ )
+   public static void main(String a[]) throws Exception
    {
-    binary.append( (val & 128) == 0 ? 0 : 1 );
-    val <<= 1;
-   }
-   System.out.println( (char)b + " \t " + b + " \t " + binary );
-   stringToBits.append( binary );
-  }
-  System.out.println( "El mensaje completo en bits es:" + stringToBits );
- }
-}
+      Socket socket = null;
+      String peticion = "";
+      String respuesta = "";
+
+      byte arreglo[] = new byte[16];
+      byte[] textoPlano;
+      byte[] textoCifrado;
+
+      ObjectInputStream ois = new ObjectInputStream(new FileInputStream("llave.ser"));
+      SecretKey clave = (SecretKey)ois.readObject();
+      ois.close();
+
+      String stringKey=clave.toString();
+      String parts[] = stringKey.split("@");
+      System.out.println( "\tLa clave es = " + parts[1] );
+  
+      //Conectando al servidor
+      try{
+        System.out.println("Me conecto al puerto "+ a[1] +" del servidor");
+        socket = new Socket(a[0],Integer.parseInt(a[1]));
+        System.out.println("Conexion exitosa!");
+        System.out.println( "\n\tMENU\nCONSULTAR\nDEPOSITAR(espacio)Cantidad\nRETIRAR(espacio)Cantidad\nTERMINAR");
+        DataOutputStream dos = new DataOutputStream( socket.getOutputStream());
+        DataInputStream dis = new DataInputStream( socket.getInputStream() );
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Cipher cifrar = Cipher.getInstance("DES/ECB/PKCS5Padding");
+        while( !peticion.equals( "terminar" ) ){
+          
+          System.out.print(">_ ");
+          //AQUI ES PARA ENVIAR
+          peticion = br.readLine();
+          textoPlano = peticion.getBytes("UTF8");
+          cifrar.init(Cipher.ENCRYPT_MODE, clave);
+          textoCifrado = cifrar.doFinal(textoPlano);
+         // System.out.println( new String(textoCifrado, "UTF8") );
+          dos.write(textoCifrado);
+          //TERMINA ENVIARR
+          //dos.writeUTF(peticion);
+
+          //AQUI ES PARA RECIBIR
+          int numeroBytesLeidos = dis.read(arreglo);
+          //System.out.println("numeroBytesLeidos: " + numeroBytesLeidos);
+          cifrar.init(Cipher.DECRYPT_MODE, clave);
+          textoPlano = cifrar.doFinal(arreglo);
+          //System.out.println( new String(textoPlano, "UTF8") );
+          respuesta = new String(textoPlano, "UTF8");
+          System.out.println("Servidor>_ " + respuesta);
+          //Termina LEEER
+
+          //respuesta = dis.readUTF();
+        }
+        dos.close();
+        dis.close();
+        socket.close();
+
+      }catch(IOException e){
+       // System.out.println("java.io.IOException generada");
+        e.printStackTrace();
+      }
+  }//Main
+
+} //Cliente
+
--- Servidor.java
+++ Servidor.java
@@ -1,68 +1,230 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Miguel Alejandro Rojas Hernandez
+ */
 import java.net.*;
 import java.io.*;
 import java.security.*;
 import javax.crypto.*;
 
-public class ServidorCifrado
+public class Servidor
 {
  public static void main(String a[]) throws Exception
  {
-  ServerSocket serverSocket = null;
-  Socket socket = null;
-  // Peticion es lo que envia el Cliente
-  String peticion = "";
-  byte arreglo[] = new byte[8];
+    ServerSocket serverSocket = null; //Crea socket servidor
+    Socket socket = null; //Crear socket
+    String peticion = ""; //CadenaCliente
+    String respuesta = ""; //CadenaServidor
+    byte[] textoPlano;
+    byte[] textoCifrado;
+    byte arreglo[] = new byte[16];
 
-  System.out.println( "Generando la llave..." );
-  KeyGenerator keyGen = KeyGenerator.getInstance("DES");
-  keyGen.init(56);
-  Key clave = keyGen.generateKey();
-  System.out.println( "la clave del servidor es=" + clave );
-  System.out.println( "Llave generada!" );
+    String saldo = "";
+    String saldoTotal = "";
+    Double actualiza;
+    int opcion;
 
-  ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("llave.ser"));
-  oos.writeObject(clave);
-  oos.close();
 
-  System.out.println("Escuchando por el puerto 8000");
-  serverSocket = new ServerSocket(8000); 
-  System.out.println("Esperando a que los clientes se conecten...");
-  socket = serverSocket.accept();
-  System.out.println("Se conecto un cliente: " + socket.getInetAddress().getHostName());
-  DataInputStream dis = new DataInputStream( socket.getInputStream() );
-  int numeroBytesLeidos = dis.read(arreglo);
-  System.out.println("numeroBytesLeidos: " + numeroBytesLeidos);
-  System.out.println( "El argumento ENCRIPTADO pasado a bytes y a bits es:" );
-  bytesToBits( arreglo );
-  System.out.println( "----------------------------------------------------" );
-  Cipher cifrar = Cipher.getInstance("DES/ECB/PKCS5Padding");
-  cifrar.init(Cipher.DECRYPT_MODE, clave);
-  byte[] textoPlano = cifrar.doFinal(arreglo);
-  System.out.println( "El argumento DESENCRIPTADO es:" );
-  System.out.println( new String(textoPlano, "UTF8") );
-  System.out.println( "El argumento DESENCRIPTADO pasado a bytes y a bits es:" );
-  bytesToBits( textoPlano );
-  peticion = new String(textoPlano, "UTF8");
-  System.out.println("El mensaje que me envio el cliente es: " + peticion);
- }
 
- public static void bytesToBits( byte[] texto )
- {
-  StringBuilder stringToBits = new StringBuilder();
-  for( int i=0; i < texto.length; i++ )
-  {
-   StringBuilder binary = new StringBuilder();
-   byte b = texto[i];
-   int val = b;
-   for( int j = 0; j < 8; j++ )
-   {
-    binary.append( (val & 128) == 0 ? 0 : 1 );
-    val <<= 1;
-   }
-   System.out.println( (char)b + " \t " + b + " \t " + binary );
-   stringToBits.append( binary );
-  }
-  System.out.println( "El mensaje completo en bits es:" + stringToBits );
- }
-}
+    
+    //Para generar la llave
+    System.out.println( "Generando la llave..." );
+    KeyGenerator keyGen = KeyGenerator.getInstance("DES");
+    keyGen.init(56);
+    SecretKey clave = keyGen.generateKey();
+    //Para mostrar solamente la clave
+    String stringKey=clave.toString();
+    String parts[] = stringKey.split("@");
+    System.out.println( "\tLa clave es = " + parts[1] );
+    System.out.println( "Llave generada!" );
+    //Escribe la llave a un archivo
+    ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("llave.ser"));
+    oos.writeObject(clave);
+    oos.close();
+    //Iniciando conexion con el socket
+    try{
+      System.out.println("Escuchando por el puerto 8000");
+      serverSocket = new ServerSocket(8000);
+    }catch(IOException e){
+      System.out.println("No se recibieron peticiones");
+      e.printStackTrace();
+    }
+
+    System.out.println("Esperando a que los clientes se conecten...");
+    while(true){
+      try{
+        socket = serverSocket.accept();
+        System.out.println("Se conecto: " + socket.getInetAddress().getHostName());
+        DataInputStream dis = new DataInputStream( socket.getInputStream() );
+        DataOutputStream dos = new DataOutputStream( socket.getOutputStream());
+        Cipher cifrar = Cipher.getInstance("DES/ECB/PKCS5Padding");
+
+        while( !respuesta.equals( "Terminar" ) ){
+          int numeroBytesLeidos = dis.read(arreglo);
+          System.out.println("numeroBytesLeidos: " + numeroBytesLeidos);
+          cifrar.init(Cipher.DECRYPT_MODE, clave);
+          textoPlano = cifrar.doFinal(arreglo);
+          //System.out.println( new String(textoPlano, "UTF8") );
+          peticion = new String(textoPlano, "UTF8");
+          //peticion = dis.readUTF();
+
+          System.out.println("Cliente>_" + peticion);
+          String comando[] = peticion.split(" ");
+
+          switch(comando[0].toLowerCase()){
+            case "consultar":
+               
+              try{
+                BufferedReader arch = new BufferedReader(new InputStreamReader(new FileInputStream("Saldo.txt")));
+                saldo = arch.readLine();
+                System.out.println(saldo);
+                arch.close();
+              }catch(IOException e){
+                System.out.println("Error al mostrar el saldo");
+                e.printStackTrace();
+              }
+              respuesta= "Saldo: "+ saldo;
+              textoPlano = respuesta.getBytes("UTF8");
+              cifrar.init(Cipher.ENCRYPT_MODE, clave);
+              textoCifrado = cifrar.doFinal(textoPlano);
+              //System.out.println( new String(textoCifrado, "UTF8") );
+              dos.write(textoCifrado);
+
+              
+
+             // dos.writeUTF(respuesta);
+                
+              break;
+            case "depositar":
+                try{
+                  BufferedReader arch = new BufferedReader(new InputStreamReader(new FileInputStream("Saldo.txt")));
+                  saldo = arch.readLine();
+                  System.out.println(saldo);
+                  arch.close();
+                }catch(IOException e){
+                    System.out.println("Error");
+                    e.printStackTrace();
+                }
+                actualiza =  Double.parseDouble(saldo)+Double.parseDouble(comando[1]);
+                saldoTotal = String.valueOf(actualiza);                          
+
+                  File fichero = new File("Saldo.txt");
+                  File fichero2= new File ("prueba2.txt");
+                try{
+                  BufferedReader arch = new BufferedReader(new InputStreamReader(new FileInputStream("Saldo.txt")));
+                  PrintWriter pw = new PrintWriter(new OutputStreamWriter(new FileOutputStream("prueba2.txt")));
+                  pw.write(saldoTotal);
+                  arch.close();
+                  pw.close();
+                }catch(IOException e){
+                    System.out.println("Error");
+                    e.printStackTrace();
+                }
+
+                if (fichero.delete())
+                     System.out.println("El fichero ha sido borrado satisfactoriamente");
+                else
+                     System.out.println("El fichero no puede ser borrado");
+                
+                if (fichero2.renameTo(fichero))
+                  System.out.println("Se renombro correctamente");
+
+                respuesta ="Dep OK: "+comando[1];
+                textoPlano = respuesta.getBytes("UTF8");
+                cifrar.init(Cipher.ENCRYPT_MODE, clave);
+                textoCifrado = cifrar.doFinal(textoPlano);
+                //System.out.println( new String(textoCifrado, "UTF8") );
+                dos.write(textoCifrado);
+
+                //dos.writeUTF(respuesta);
+
+              break;
+            case "retirar":
+              try{
+                  BufferedReader arch = new BufferedReader(new InputStreamReader(new FileInputStream("Saldo.txt")));
+                  saldo = arch.readLine();
+                  System.out.println(saldo);
+                  arch.close();
+                }catch(IOException e){
+                    System.out.println("Error");
+                    e.printStackTrace();
+                }
+                actualiza =  Double.parseDouble(saldo)-Double.parseDouble(comando[1]);
+                saldoTotal = String.valueOf(actualiza);                          
+
+                  File fichero3 = new File("Saldo.txt");
+                  File fichero4= new File ("prueba2.txt");
+                try{
+                  BufferedReader arch = new BufferedReader(new InputStreamReader(new FileInputStream("Saldo.txt")));
+                  PrintWriter pw = new PrintWriter(new OutputStreamWriter(new FileOutputStream("prueba2.txt")));
+                  pw.write(saldoTotal);
+                  arch.close();
+                  pw.close();
+                }catch(IOException e){
+                    System.out.println("Error");
+                    e.printStackTrace();
+                }
+
+                if (fichero3.delete())
+                     System.out.println("El fichero ha sido borrado satisfactoriamente");
+                else
+                     System.out.println("El fichero no puede ser borrado");
+                
+                if (fichero4.renameTo(fichero3))
+                  System.out.println("Se renombro correctamente");
+
+                
+                respuesta ="Ret OK: "+comando[1];
+                textoPlano = respuesta.getBytes("UTF8");
+                cifrar.init(Cipher.ENCRYPT_MODE, clave);
+                textoCifrado = cifrar.doFinal(textoPlano);
+                //System.out.println( new String(textoCifrado, "UTF8") );
+                dos.write(textoCifrado);
+
+//                dos.writeUTF(respuesta);
+               
+              break;
+            case "terminar":
+              respuesta ="Terminar";
+              textoPlano = respuesta.getBytes("UTF8");
+              cifrar.init(Cipher.ENCRYPT_MODE, clave);
+              textoCifrado = cifrar.doFinal(textoPlano);
+              System.out.println( new String(textoCifrado, "UTF8") );
+              dos.write(textoCifrado);
+
+             // dos.writeUTF(respuesta);
+
+              break;
+            default:
+              respuesta ="Op Invalida";
+              textoPlano = respuesta.getBytes("UTF8");
+              cifrar.init(Cipher.ENCRYPT_MODE, clave);
+              textoCifrado = cifrar.doFinal(textoPlano);
+              System.out.println( new String(textoCifrado, "UTF8") );
+              dos.write(textoCifrado);
+
+              break;
+          }
+        }
+
+ dos.close();
+ dis.close();
+ socket = null;
+ System.out.println("Conexion Terminada");
+ System.exit(0);
+      }catch(IOException e){
+        System.out.println("Aqui esta el erro");
+        e.printStackTrace();
+      }
+    }//WHILE TRUE
+
+  }//Main
+
+}//Clase Servidor
 
